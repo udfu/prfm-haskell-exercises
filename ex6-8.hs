b8b60f7577972df491a1133c0e9a2ea3fe09a4c0
udfu <dario_delpiero10@hotmail.com>
2020-07-25 13:32:23 +0200

    add(ex6) show how recursion is evaluated

--- ex6-8.hs
+++ ex6-8.hs
@@ -21,3 +21,38 @@ euclid m n | m == n = m
            | m < n = euclid (n-m) m
            | m > n = euclid (m-n) n
 
+-- 5
+{-
+length [1,2,3]
+= { applying length }
+1 + length [2,3]
+= { applying length }
+1 + 1 + length [3]
+= { applying length }
+1 + 1 + 1 length []
+= { applying length }
+1 + 1 + 1 + 0
+= { applying + }
+3
+
+drop 3 [1,2,3,4,5]
+= { applying drop }
+drop 2 [2,3,4,5]
+= { applying drop }
+drop 1 [3,4,5]
+= { applying drop }
+drop 0 [4,5]
+= { applying drop }
+[4,5]
+
+init [1,2,3]
+= { applying init }
+1 : init [2,3]
+= { applying init }
+1 : (2 : init [3])
+= { applying init }
+1 : (2: ([]))
+= list notation
+[1,2]
+-}
+
